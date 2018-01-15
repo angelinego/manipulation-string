72f60a71a4c9fdb4c70f44d40dd3e1d443483703
Angeline Go <angeline_go87@hotmail.com>
2018-01-15 13:44:40 +0700

    first assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("LEVEL 0")
+const sentenceA = "Hello World!"
+const sentenceB = "Good Bye"
+
+console.log(sentenceA)
+console.log(sentenceB)
+console.log("--------------------------------------------------------------------------");
+
+console.log("LEVEL 1")
+const sentenceC = sentenceA + " " + sentenceB
+const sentenceD = `${sentenceA} ${sentenceB}`
+
+console.log(sentenceC)
+console.log(sentenceD)
+
+console.log("LEVEL 2")
+console.log(sentenceA.toUpperCase())
+console.log(sentenceB.toUpperCase())
+console.log(sentenceA.toLowerCase())
+console.log(sentenceB.toLowerCase())
+console.log("--------------------------------------------------------------------------");
+
+console.log("LEVEL3")
+var sentenceE = "hello goodness" // "Hello Goodness"
+console.log(sentenceD.toTitleCase())
